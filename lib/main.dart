cf7fec47cf2252b4fdba7d0d31528724283e5a1d
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2019-05-29 22:06:29 +0530

    Stateful Widget of the hello world app

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,27 @@ import 'package:flutter/material.dart';
       Widget build(BuildContext context) {
         return new MaterialApp(
           title: "My App",
-          home:new Scaffold(
-            appBar: new AppBar(
-              title: new Text("Home Page"),
-            ),
-            body: new Center(child :new Text("Welcome World")),
-          )
+          home:new HomePage(),
         );
       }
     }
+    
+    class HomePage extends StatefulWidget {
+      @override
+      _HomePageState createState() => _HomePageState();
+    }
+    
+    class _HomePageState extends State<HomePage> {
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: new AppBar(
+            title: new Text("Home Page"),
+          ),
+          body: new Center( child: new Text("Hello Stateful widget"),),
+        );
+      }
+    }
+    
+    
     
