3a67eaa43f62e0b644b36dfbe846f2a2c1e7d9a8
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2019-05-30 17:58:51 +0530

    changing text on button click

--- lib/main.dart
+++ lib/main.dart
@@ -11,20 +11,53 @@ import 'package:flutter/material.dart';
         );
       }
     }
-    
+
+
     class HomePage extends StatefulWidget {
       @override
       _HomePageState createState() => _HomePageState();
     }
-    
+
     class _HomePageState extends State<HomePage> {
+
+
+      String myText="hello world";
+      void _changeText(){
+        setState(() {
+          if(myText.startsWith("h")) {
+            myText = "Welcome to hello world";
+          }
+          else
+            {
+              myText="hello world";
+            }
+        });
+      }
+
+      Widget _bodyWidget(){
+        return new Container(
+          padding: const EdgeInsets.all(8.0),
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Text(myText),
+                new RaisedButton(
+                  child: new Text("Click"),
+                  onPressed:_changeText,
+                )
+              ],
+            ),
+          ),
+        );
+      }
+
       @override
       Widget build(BuildContext context) {
         return Scaffold(
           appBar: new AppBar(
             title: new Text("Home Page"),
           ),
-          body: new Center( child: new Text("Hello Stateful widget"),),
+          body: _bodyWidget(),
         );
       }
     }
